993d88ef335d7ced7e39584375e1bae0b8fb8299
Richard Albert Leddy <richardaleddy@gmail.com>
2019-08-16 21:36:52 -0700

    up to date

---
+++ console-test/test.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char **argv) {
+	cout << "this is a test" << endl;
+	
+	clock_t start = clock();
+	unsigned long int N = 11000000L; //  UINT_MAX;
+	for ( unsigned long int i = 0; i < N; i++ ) {
+		// do nothing
+	}
+	
+	clock_t stop = clock();
+	
+	cout << "N = " << N << endl;
+	cout << (double(stop - start)*1.0/1000000.0) << endl;
+
+}
